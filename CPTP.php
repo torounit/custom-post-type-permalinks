2d4180ccafda160b1e5ac773ce3f10e036587aec
Toro_Unit <mail@torounit.com>
2014-01-20 06:14:18 +0900

    add action hook CPTP_load_modules.

--- CPTP.php
+++ CPTP.php
@@ -31,14 +31,41 @@ class CPTP {
 	private static $instance;
 
 	private function __construct() {
+		$this->load_modules();
+		$this->init();
+	}
 
+	/**
+	 *
+	 * load_modules
+	 *
+	 * Load CPTP_Modules.
+	 * @since 0.9.5
+	 *
+	 * */
+
+	private function load_modules() {
 		new CPTP_Module_Setting();
 		new CPTP_Module_Rewrite();
 		new CPTP_Module_Admin();
 		new CPTP_Module_Permalink();
 		new CPTP_Module_GetArchives();
 		new CPTP_Module_FlushRules();
+		do_action( "CPTP_load_modules" );
 
+	}
+
+	/**
+	 *
+	 * init
+	 *
+	 * Fire Module::add_hook
+	 *
+	 * @since 0.9.5
+	 *
+	 * */
+
+	private function init() {
 		do_action( "CPTP_init" );
 	}
 
