e87740f2c311ed68abde92cf738d6ab1b4d397c7
Toro_Unit <mail@torounit.com>
2014-01-23 19:33:19 +0900

    archive link error fix in 3.8

--- CPTP/Module/Rewrite.php
+++ CPTP/Module/Rewrite.php
@@ -15,60 +15,9 @@ class CPTP_Module_Rewrite extends CPTP_Module {
 
 	public function add_hook() {
 		add_action( 'parse_request', array( $this, "parse_request") );
-		add_action( 'wp_loaded', array( $this,'add_archive_rewrite_rules'), 99 );
-		add_action( 'wp_loaded', array( $this,'add_tax_rewrite_rules') );
 		add_action( 'registered_post_type', array( $this,'registered_post_type'), 10, 2 );
+		add_action( 'registered_taxonomy', array( $this,'registered_taxonomy'), 10, 3 );
 	}
-
-	/**
-	 *
-	 * Add rewrite rules for archives.
-	 * @version 1.1
-	 *
-	 */
-	public function add_archive_rewrite_rules() {
-		$post_types = CPTP_Util::get_post_types();
-
-		foreach ( $post_types as $post_type ):
-			if( !$post_type ) {
-				continue;
-			}
-
-			$permalink = get_option( $post_type.'_structure' );
-			$post_type_obj = get_post_type_object($post_type);
-			$slug = $post_type_obj->rewrite['slug'];
-			if( !$slug )
-				$slug = $post_type;
-
-			if( $post_type_obj->has_archive ){
-				if( is_string( $post_type_obj->has_archive ) ){
-					$slug = $post_type_obj->has_archive;
-				};
-
-				if($post_type_obj->rewrite['with_front']) {
-					global $wp_rewrite;
-					$slug = substr( $wp_rewrite->front, 1 ).$slug;
-				}
-
-
-				add_rewrite_rule( $slug.'/date/([0-9]{4})/([0-9]{1,2})/([0-9]{1,2})/feed/(feed|rdf|rss|rss2|atom)/?$', 'index.php?year=$matches[1]&monthnum=$matches[2]&day=$matches[3]&feed=$matches[4]&post_type='.$post_type, 'top' );
-				add_rewrite_rule( $slug.'/date/([0-9]{4})/([0-9]{1,2})/([0-9]{1,2})/(feed|rdf|rss|rss2|atom)/?$', 'index.php?year=$matches[1]&monthnum=$matches[2]&day=$matches[3]&feed=$matches[4]&post_type='.$post_type, 'top' );
-				add_rewrite_rule( $slug.'/date/([0-9]{4})/([0-9]{1,2})/([0-9]{1,2})/page/?([0-9]{1,})/?$', 'index.php?year=$matches[1]&monthnum=$matches[2]&day=$matches[3]&paged=$matches[4]&post_type='.$post_type, 'top' );
-				add_rewrite_rule( $slug.'/date/([0-9]{4})/([0-9]{1,2})/([0-9]{1,2})/?$', 'index.php?year=$matches[1]&monthnum=$matches[2]&day=$matches[3]&post_type='.$post_type, 'top' );
-				add_rewrite_rule( $slug.'/date/([0-9]{4})/([0-9]{1,2})/feed/(feed|rdf|rss|rss2|atom)/?$', 'index.php?year=$matches[1]&monthnum=$matches[2]&feed=$matches[3]&post_type='.$post_type, 'top' );
-				add_rewrite_rule( $slug.'/date/([0-9]{4})/([0-9]{1,2})/(feed|rdf|rss|rss2|atom)/?$', 'index.php?year=$matches[1]&monthnum=$matches[2]&feed=$matches[3]&post_type='.$post_type, 'top' );
-				add_rewrite_rule( $slug.'/date/([0-9]{4})/([0-9]{1,2})/page/?([0-9]{1,})/?$', 'index.php?year=$matches[1]&monthnum=$matches[2]&paged=$matches[3]&post_type='.$post_type, 'top' );
-				add_rewrite_rule( $slug.'/date/([0-9]{4})/([0-9]{1,2})/?$', 'index.php?year=$matches[1]&monthnum=$matches[2]&post_type='.$post_type, 'top' );
-				add_rewrite_rule( $slug.'/date/([0-9]{4})/feed/(feed|rdf|rss|rss2|atom)/?$', 'index.php?year=$matches[1]&feed=$matches[2]&post_type='.$post_type, 'top' );
-				add_rewrite_rule( $slug.'/date/([0-9]{4})/(feed|rdf|rss|rss2|atom)/?$', 'index.php?year=$matches[1]&feed=$matches[2]&post_type='.$post_type, 'top' );
-				add_rewrite_rule( $slug.'/date/([0-9]{4})/page/?([0-9]{1,})/?$', 'index.php?year=$matches[1]&paged=$matches[2]&post_type='.$post_type, 'top' );
-				add_rewrite_rule( $slug.'/date/([0-9]{4})/?$', 'index.php?year=$matches[1]&post_type='.$post_type, 'top' );
-				add_rewrite_rule( $slug.'/author/([^/]+)/?$', 'index.php?author_name=$matches[1]&post_type='.$post_type, 'top' );
-			}
-
-		endforeach;
-	}
-
 
 
 	/**
@@ -82,7 +31,7 @@ class CPTP_Module_Rewrite extends CPTP_Module {
 
 	public function registered_post_type( $post_type, $args ) {
 
-		global $wp_post_types, $wp_rewrite, $wp;
+		global $wp_post_types, $wp_rewrite;
 
 		if( $args->_builtin or !$args->publicly_queryable or !$args->show_ui ){
 			return false;
@@ -108,80 +57,99 @@ class CPTP_Module_Rewrite extends CPTP_Module {
 		$rewrite_args["walk_dirs"] = false;
 		add_permastruct( $post_type, $permalink, $rewrite_args);
 
+
+
+		$slug = $args->rewrite['slug'];
+		if( $args->has_archive ){
+			if( is_string( $args->has_archive ) ){
+				$slug = $args->has_archive;
+			};
+
+			if($args->rewrite['with_front']) {
+				$slug = substr( $wp_rewrite->front, 1 ).$slug;
+			}
+
+			add_rewrite_rule( $slug.'/date/([0-9]{4})/([0-9]{1,2})/([0-9]{1,2})/feed/(feed|rdf|rss|rss2|atom)/?$', 'index.php?year=$matches[1]&monthnum=$matches[2]&day=$matches[3]&feed=$matches[4]&post_type='.$post_type, 'top' );
+			add_rewrite_rule( $slug.'/date/([0-9]{4})/([0-9]{1,2})/([0-9]{1,2})/(feed|rdf|rss|rss2|atom)/?$', 'index.php?year=$matches[1]&monthnum=$matches[2]&day=$matches[3]&feed=$matches[4]&post_type='.$post_type, 'top' );
+			add_rewrite_rule( $slug.'/date/([0-9]{4})/([0-9]{1,2})/([0-9]{1,2})/page/?([0-9]{1,})/?$', 'index.php?year=$matches[1]&monthnum=$matches[2]&day=$matches[3]&paged=$matches[4]&post_type='.$post_type, 'top' );
+			add_rewrite_rule( $slug.'/date/([0-9]{4})/([0-9]{1,2})/([0-9]{1,2})/?$', 'index.php?year=$matches[1]&monthnum=$matches[2]&day=$matches[3]&post_type='.$post_type, 'top' );
+			add_rewrite_rule( $slug.'/date/([0-9]{4})/([0-9]{1,2})/feed/(feed|rdf|rss|rss2|atom)/?$', 'index.php?year=$matches[1]&monthnum=$matches[2]&feed=$matches[3]&post_type='.$post_type, 'top' );
+			add_rewrite_rule( $slug.'/date/([0-9]{4})/([0-9]{1,2})/(feed|rdf|rss|rss2|atom)/?$', 'index.php?year=$matches[1]&monthnum=$matches[2]&feed=$matches[3]&post_type='.$post_type, 'top' );
+			add_rewrite_rule( $slug.'/date/([0-9]{4})/([0-9]{1,2})/page/?([0-9]{1,})/?$', 'index.php?year=$matches[1]&monthnum=$matches[2]&paged=$matches[3]&post_type='.$post_type, 'top' );
+			add_rewrite_rule( $slug.'/date/([0-9]{4})/([0-9]{1,2})/?$', 'index.php?year=$matches[1]&monthnum=$matches[2]&post_type='.$post_type, 'top' );
+			add_rewrite_rule( $slug.'/date/([0-9]{4})/feed/(feed|rdf|rss|rss2|atom)/?$', 'index.php?year=$matches[1]&feed=$matches[2]&post_type='.$post_type, 'top' );
+			add_rewrite_rule( $slug.'/date/([0-9]{4})/(feed|rdf|rss|rss2|atom)/?$', 'index.php?year=$matches[1]&feed=$matches[2]&post_type='.$post_type, 'top' );
+			add_rewrite_rule( $slug.'/date/([0-9]{4})/page/?([0-9]{1,})/?$', 'index.php?year=$matches[1]&paged=$matches[2]&post_type='.$post_type, 'top' );
+			add_rewrite_rule( $slug.'/date/([0-9]{4})/?$', 'index.php?year=$matches[1]&post_type='.$post_type, 'top' );
+			add_rewrite_rule( $slug.'/author/([^/]+)/?$', 'index.php?author_name=$matches[1]&post_type='.$post_type, 'top' );
+		}
+
+
+
 	}
 
-	/**
-	 *
-	 * Add rewrite rules for custom taxonomies.
-	 * @since 0.6
-	 * @version 2.1
-	 *
-	 */
-	public function add_tax_rewrite_rules() {
+
+	public function registered_taxonomy( $taxonomy, $object_type, $args ) {
+		print_r($args);
+
 		if(get_option('no_taxonomy_structure')) {
 			return false;
 		}
-
-
-		global $wp_rewrite;
-		$taxonomies = CPTP_Util::get_taxonomies();
-		$taxonomies['category'] = 'category';
-
-		if(empty($taxonomies)) {
+		if($args["_builtin"]) {
 			return false;
 		}
 
-		foreach ($taxonomies as $taxonomy) :
-			$taxonomyObject = get_taxonomy($taxonomy);
-			$post_types = $taxonomyObject->object_type;
+		global $wp_rewrite;
 
-			foreach ($post_types as $post_type):
-				$post_type_obj = get_post_type_object($post_type);
-				$slug = $post_type_obj->rewrite['slug'];
+		$taxonomyObject = $args;
+		$post_types = $args["object_type"];
+		foreach ($post_types as $post_type):
+			$post_type_obj = get_post_type_object($post_type);
+			$slug = $post_type_obj->rewrite['slug'];
 
 
-				if(!$slug) {
-					$slug = $post_type;
-				}
+			if(!$slug) {
+				$slug = $post_type;
+			}
 
-				if(is_string($post_type_obj->has_archive)) {
-					$slug = $post_type_obj->has_archive;
-				};
+			if(is_string($post_type_obj->has_archive)) {
+				$slug = $post_type_obj->has_archive;
+			};
 
-				if($post_type_obj->rewrite['with_front']) {
-					$slug = substr( $wp_rewrite->front, 1 ).$slug;
-				}
+			if($post_type_obj->rewrite['with_front']) {
+				$slug = substr( $wp_rewrite->front, 1 ).$slug;
+			}
 
-				if ( $taxonomy == 'category' ){
-					$taxonomypat = ($cb = get_option('category_base')) ? $cb : $taxonomy;
-					$tax = 'category_name';
+			if ( $taxonomy == 'category' ){
+				$taxonomypat = ($cb = get_option('category_base')) ? $cb : $taxonomy;
+				$tax = 'category_name';
+			} else {
+				// Edit by [Xiphe]
+				if (isset($args["rewrite"]['slug'])) {
+					$taxonomypat = $args["rewrite"]['slug'];
 				} else {
-					// Edit by [Xiphe]
-					if (isset($taxonomyObject->rewrite['slug'])) {
-						$taxonomypat = $taxonomyObject->rewrite['slug'];
-					} else {
-						$taxonomypat = $taxonomy;
-					}
-					// [Xiphe] stop
-
-					$tax = $taxonomy;
+					$taxonomypat = $taxonomy;
 				}
+				// [Xiphe] stop
+
+				$tax = $taxonomy;
+			}
 
 
-				//add taxonomy slug
-				add_rewrite_rule( $slug.'/'.$taxonomypat.'/(.+?)/page/?([0-9]{1,})/?$', 'index.php?'.$tax.'=$matches[1]&paged=$matches[2]&post_type='.$post_type, 'top' );
-				add_rewrite_rule( $slug.'/'.$taxonomypat.'/(.+?)/feed/(feed|rdf|rss|rss2|atom)/?$', 'index.php?'.$tax.'=$matches[1]&feed=$matches[2]&post_type='.$post_type, 'top' );
-				add_rewrite_rule( $slug.'/'.$taxonomypat.'/(.+?)/(feed|rdf|rss|rss2|atom)/?$', 'index.php?'.$tax.'=$matches[1]&feed=$matches[2]&post_type='.$post_type, 'top' );
-				add_rewrite_rule( $slug.'/'.$taxonomypat.'/(.+?)/?$', 'index.php?'.$tax.'=$matches[1]&post_type='.$post_type, 'top' );  // modified by [steve] [*** bug fixing]
+			//add taxonomy slug
+			add_rewrite_rule( $slug.'/'.$taxonomypat.'/(.+?)/page/?([0-9]{1,})/?$', 'index.php?'.$tax.'=$matches[1]&paged=$matches[2]&post_type='.$post_type, 'top' );
+			add_rewrite_rule( $slug.'/'.$taxonomypat.'/(.+?)/feed/(feed|rdf|rss|rss2|atom)/?$', 'index.php?'.$tax.'=$matches[1]&feed=$matches[2]&post_type='.$post_type, 'top' );
+			add_rewrite_rule( $slug.'/'.$taxonomypat.'/(.+?)/(feed|rdf|rss|rss2|atom)/?$', 'index.php?'.$tax.'=$matches[1]&feed=$matches[2]&post_type='.$post_type, 'top' );
+			add_rewrite_rule( $slug.'/'.$taxonomypat.'/(.+?)/?$', 'index.php?'.$tax.'=$matches[1]&post_type='.$post_type, 'top' );  // modified by [steve] [*** bug fixing]
 
-				// below rules were added by [steve]
-				add_rewrite_rule( $taxonomypat.'/(.+?)/date/([0-9]{4})/([0-9]{1,2})/?$', 'index.php?'.$tax.'=$matches[1]&year=$matches[2]&monthnum=$matches[3]&post_type='.$post_type, 'top' );
-				add_rewrite_rule( $taxonomypat.'/(.+?)/date/([0-9]{4})/([0-9]{1,2})/page/?([0-9]{1,})/?$', 'index.php?'.$tax.'=$matches[1]&year=$matches[2]&monthnum=$matches[3]&paged=$matches[4]&post_type='.$post_type, 'top' );
-				add_rewrite_rule( $slug.'/'.$taxonomypat.'/(.+?)/date/([0-9]{4})/([0-9]{1,2})/?$', 'index.php?'.$tax.'=$matches[1]&year=$matches[2]&monthnum=$matches[3]&post_type='.$post_type, 'top' );
-				add_rewrite_rule( $slug.'/'.$taxonomypat.'/(.+?)/date/([0-9]{4})/([0-9]{1,2})/page/?([0-9]{1,})/?$', 'index.php?'.$tax.'=$matches[1]&year=$matches[2]&monthnum=$matches[3]&paged=$matches[4]&post_type='.$post_type, 'top' );
+			// below rules were added by [steve]
+			add_rewrite_rule( $taxonomypat.'/(.+?)/date/([0-9]{4})/([0-9]{1,2})/?$', 'index.php?'.$tax.'=$matches[1]&year=$matches[2]&monthnum=$matches[3]&post_type='.$post_type, 'top' );
+			add_rewrite_rule( $taxonomypat.'/(.+?)/date/([0-9]{4})/([0-9]{1,2})/page/?([0-9]{1,})/?$', 'index.php?'.$tax.'=$matches[1]&year=$matches[2]&monthnum=$matches[3]&paged=$matches[4]&post_type='.$post_type, 'top' );
+			add_rewrite_rule( $slug.'/'.$taxonomypat.'/(.+?)/date/([0-9]{4})/([0-9]{1,2})/?$', 'index.php?'.$tax.'=$matches[1]&year=$matches[2]&monthnum=$matches[3]&post_type='.$post_type, 'top' );
+			add_rewrite_rule( $slug.'/'.$taxonomypat.'/(.+?)/date/([0-9]{4})/([0-9]{1,2})/page/?([0-9]{1,})/?$', 'index.php?'.$tax.'=$matches[1]&year=$matches[2]&monthnum=$matches[3]&paged=$matches[4]&post_type='.$post_type, 'top' );
 
-			endforeach;
 		endforeach;
+
 	}
 
 
