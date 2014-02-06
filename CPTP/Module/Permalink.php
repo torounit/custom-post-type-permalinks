85440da33928070a4707b7d88bc5da4b576eaa3f
清野 真衣子 <maikoseino@gmail.com>
2014-02-06 21:27:32 +0900

    複数のカスタム投稿に対して横断的に使用するタームのリンクが常に単一のものしか取れない問題について修正しました。
    表示しているカスタム投稿から、取得するスラッグを調整し、
    意図する挙動となるような形としました。

--- CPTP/Module/Permalink.php
+++ CPTP/Module/Permalink.php
@@ -218,7 +218,13 @@ class CPTP_Module_Permalink extends CPTP_Module {
 
 		$wp_home = rtrim( home_url(), '/' );
 
-		$post_type = $taxonomy->object_type[0];
+		global $qobj;
+		if(in_array($qobj->rewrite['slug'], $taxonomy->object_type)){
+			$post_type = $qobj->rewrite['slug'];
+		}
+		else {
+			$post_type = $taxonomy->object_type[0];
+		}
 		$post_type_obj = get_post_type_object($post_type);
 		$slug = $post_type_obj->rewrite['slug'];
 		$with_front = $post_type_obj->rewrite['with_front'];
