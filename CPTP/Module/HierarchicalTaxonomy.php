61607ae16de619cc9697da14994004ddcd0508e6
Hiroshi Urabe <mail@torounit.com>
2014-10-12 22:36:27 +0900

    hierarchal tax

--- CPTP/Module/HierarchicalTaxonomy.php
+++ CPTP/Module/HierarchicalTaxonomy.php
@@ -13,60 +13,31 @@
 
 class CPTP_Module_HierarchicalTaxonomy extends CPTP_Module {
 
+	private $fixed_taxonomies;
+
 	public function add_hook() {
-		if(get_option( "fix_hierarchical_taxonomy_permalink")) {
-			add_action( 'parse_request', array( $this, "parse_request") );
-			add_filter( 'term_link', array( $this,'term_link'), 10, 3 );
+		$this->fixed_taxonomies = array();
+		add_action( 'registered_taxonomy', array( $this, "registered_taxonomy"), 10, 3 );
 
-		}
 	}
 
+	public function registered_taxonomy( $taxonomy, $object_type, $args ) {
+		if( !isset($this->fixed_taxonomies[$taxonomy]) ) {
 
-	/**
-	 *
-	 * Fix taxonomy = parent/child => taxonomy => child
-	 * @since 0.9.3
-	 *
-	 */
-	public function parse_request($obj) {
-		$taxes = CPTP_Util::get_taxonomies();
-		foreach ($taxes as $key => $tax) {
-			if(isset($obj->query_vars[$tax])) {
-				if(strpos( $obj->query_vars[$tax] ,"/") !== false ) {
-					$query_vars = explode("/", $obj->query_vars[$tax]);
-					if(is_array($query_vars)) {
-						$obj->query_vars[$tax] = array_pop($query_vars);
-					}
-				}
+			$this->fixed_taxonomies[$taxonomy] = true;
+
+			if( get_option( "fix_hierarchical_taxonomy_permalink") and $args["hierarchical"] ) {
+				$args["rewrite"]["hierarchical"] = true;
 			}
+
+			register_taxonomy( $taxonomy, $object_type, $args );
+
 		}
 	}
 
 
-	/**
-	 *
-	 * Fix taxonomy link outputs.
-	 * @since 0.6
-	 * @version 1.0
-	 *
-	 */
-	public function term_link( $termlink, $term, $taxonomy ) {
-		global $wp_rewrite;
-
-		$taxonomy = get_taxonomy($taxonomy);
-		if( $taxonomy->_builtin )
-			return $termlink;
-
-		if( empty($taxonomy) )
-			return $termlink;
 
 
-		if ( ! $taxonomy->rewrite['hierarchical'] ) {
-			$termlink = str_replace( $term->slug.'/', CPTP_Util::get_taxonomy_parents( $term->term_id,$taxonomy->name, false, '/', true ), $termlink );
-		}
-
-		return $termlink;
-	}
 
 
 
