1a80cb298c02d7bc0c56ae89ad64d47afec8f688
Toro_Unit <mail@torounit.com>
2014-12-21 01:16:54 +0900

    FlushRules uninstall hook

--- CPTP/Module/FlushRules.php
+++ CPTP/Module/FlushRules.php
@@ -15,23 +15,12 @@ class CPTP_Module_FlushRules extends CPTP_Module {
 	public function add_hook() {
 		add_action( 'init', array( $this, 'update_rules') );
 		add_action( 'update_option_cptp_version', array( $this, 'update_rules') );
-		add_action( 'wp_loaded', array( $this, "dequeue_flush_rules"),100);
+		add_action( 'wp_loaded', array( __CLASS__, "dequeue_flush_rules"),100);
+
+		register_activation_hook( CPTP_PLUGIN_FILE, array( __CLASS__, 'queue_flush_rules') );
+		register_uninstall_hook( CPTP_PLUGIN_FILE, array( __CLASS__, 'uninstall_hook') );
 	}
 
-	/**
-	 *
-	 * dequeue flush rules
-	 * @since 0.9
-	 *
-	 */
-
-	public function dequeue_flush_rules () {
-		if(get_option("queue_flush_rules")){
-			flush_rewrite_rules();
-			update_option( "queue_flush_rules", 0 );
-
-		}
-	}
 
 
 	/**
@@ -43,12 +32,26 @@ class CPTP_Module_FlushRules extends CPTP_Module {
 	public function update_rules() {
 
 		$post_types = CPTP_Util::get_post_types();
-		$type_count = count($post_types);
-		$i = 0;
-		foreach ($post_types as $post_type):
-			add_action('update_option_'.$post_type.'_structure',array( $this,'queue_flush_rules'),10,2);
+		foreach ( $post_types as $post_type ):
+			add_action( 'update_option_'.$post_type.'_structure', array( __CLASS__, 'queue_flush_rules'), 10, 2 );
 		endforeach;
-		add_action('update_option_no_taxonomy_structure',array( $this,'queue_flush_rules'),10,2);
+		add_action( 'update_option_no_taxonomy_structure', array( __CLASS__, 'queue_flush_rules'), 10, 2 );
+	}
+
+
+	/**
+	 *
+	 * dequeue flush rules
+	 * @since 0.9
+	 *
+	 */
+
+	public static function dequeue_flush_rules () {
+		if(get_option("queue_flush_rules")){
+			flush_rewrite_rules();
+			update_option( "queue_flush_rules", 0 );
+
+		}
 	}
 
 
@@ -60,8 +63,12 @@ class CPTP_Module_FlushRules extends CPTP_Module {
 	 *
 	 */
 
-	public function queue_flush_rules(){
+	public static function queue_flush_rules(){
 		update_option( "queue_flush_rules", 1 );
+	}
+
+	public static function uninstall_hook() {
+		delete_option( "queue_flush_rules" );
 	}
 
 }
