3e9e7727d9ad77fdab4ba93e1da5dcbe52839944
Hiroshi Urabe <mail@torounit.com>
2015-01-13 12:32:22 +0900

    same fix for wpcs

--- CPTP/Module/Option.php
+++ CPTP/Module/Option.php
@@ -14,47 +14,49 @@
 class CPTP_Module_Option extends CPTP_Module {
 
 	public function add_hook() {
-		add_action( 'admin_init', array( $this,'save_options'), 30 );
-		register_uninstall_hook( CPTP_PLUGIN_FILE, array( __CLASS__, 'uninstall_hook') );
+		add_action( 'admin_init', array( $this, 'save_options' ), 30 );
+		register_uninstall_hook( CPTP_PLUGIN_FILE, array( __CLASS__, 'uninstall_hook' ) );
 	}
 
 	public function save_options() {
 
-		if(isset($_POST['submit']) and isset($_POST['_wp_http_referer'])){
-			if( strpos($_POST['_wp_http_referer'],'options-permalink.php') !== FALSE ) {
+		if ( isset( $_POST['submit'] ) and isset( $_POST['_wp_http_referer'] ) ){
+			if ( false !== strpos( $_POST['_wp_http_referer'], 'options-permalink.php' ) ) {
 				$post_types = CPTP_Util::get_post_types();
-				foreach ($post_types as $post_type):
+				foreach ( $post_types as $post_type ):
 
-					$structure = trim(esc_attr($_POST[$post_type.'_structure']));#get setting
+					$structure = trim( esc_attr( $_POST[ $post_type.'_structure' ] ) );#get setting
 
 					#default permalink structure
-					if( !$structure )
+					if ( ! $structure ) {
 						$structure = CPTP_DEFAULT_PERMALINK;
+					}
 
-					$structure = str_replace('//','/','/'.$structure);# first "/"
+					$structure = str_replace( '//', '/', '/'.$structure );# first "/"
 					#last "/"
-					$lastString = substr(trim(esc_attr($_POST['permalink_structure'])),-1);
-					$structure = rtrim($structure,'/');
+					$lastString = substr( trim( esc_attr( $_POST['permalink_structure'] ) ), -1 );
+					$structure = rtrim( $structure, '/' );
 
-					if ( $lastString == '/')
+					if ( $lastString == '/' ) {
 						$structure = $structure.'/';
+					}
 
-					update_option($post_type.'_structure', $structure );
+					update_option( $post_type.'_structure', $structure );
 				endforeach;
 			}
 
-			if(!isset($_POST['no_taxonomy_structure'])){
+			if ( ! isset( $_POST['no_taxonomy_structure'] ) ) {
 				$set = true;
 			}else {
 				$set = false;
 			}
-			update_option('no_taxonomy_structure', $set);
+			update_option( 'no_taxonomy_structure', $set );
 
 		}
 	}
 
 	public static function uninstall_hook() {
-		foreach (CPTP_Util::get_post_types() as $post_type) {
+		foreach ( CPTP_Util::get_post_types() as $post_type ) {
 			delete_option( $post_type.'_structure' );
 		}
 
