772279c40021f2d6a9c89b13914210f9a65730dd
Hiroshi Urabe <mail@torounit.com>
2015-03-30 17:25:16 +0900

    Revert "カスタムタクソノミーアーカイブにカスタム投稿タイプのパラメータを追加。"
    This reverts commit 94cc0595bb2e57409f86c39ce82697fb2cb25279.

--- CPTP/Module/Rewrite.php
+++ CPTP/Module/Rewrite.php
@@ -170,17 +170,16 @@ class CPTP_Module_Rewrite extends CPTP_Module {
 			}
 
 			//add taxonomy slug
-			add_rewrite_rule( $slug.'/'.$taxonomy_slug.'/(.+?)/page/?([0-9]{1,})/?$', 'index.php?'.$taxonomy_key.'=$matches[1]&paged=$matches[2]&post_type='.$post_type, 'top' );
-			add_rewrite_rule( $slug.'/'.$taxonomy_slug.'/(.+?)/feed/(feed|rdf|rss|rss2|atom)/?$', 'index.php?'.$taxonomy_key.'=$matches[1]&feed=$matches[2]&post_type='.$post_type, 'top' );
-			add_rewrite_rule( $slug.'/'.$taxonomy_slug.'/(.+?)/(feed|rdf|rss|rss2|atom)/?$', 'index.php?'.$taxonomy_key.'=$matches[1]&feed=$matches[2]&post_type='.$post_type, 'top' );
-			add_rewrite_rule( $slug.'/'.$taxonomy_slug.'/(.+?)/?$', 'index.php?'.$taxonomy_key.'=$matches[1]&post_type='.$post_type, 'top' );  // modified by [steve] [*** bug fixing]
+			add_rewrite_rule( $slug.'/'.$taxonomy_slug.'/(.+?)/page/?([0-9]{1,})/?$', 'index.php?'.$taxonomy_key.'=$matches[1]&paged=$matches[2]', 'top' );
+			add_rewrite_rule( $slug.'/'.$taxonomy_slug.'/(.+?)/feed/(feed|rdf|rss|rss2|atom)/?$', 'index.php?'.$taxonomy_key.'=$matches[1]&feed=$matches[2]', 'top' );
+			add_rewrite_rule( $slug.'/'.$taxonomy_slug.'/(.+?)/(feed|rdf|rss|rss2|atom)/?$', 'index.php?'.$taxonomy_key.'=$matches[1]&feed=$matches[2]', 'top' );
+			add_rewrite_rule( $slug.'/'.$taxonomy_slug.'/(.+?)/?$', 'index.php?'.$taxonomy_key.'=$matches[1]', 'top' );  // modified by [steve] [*** bug fixing]
 
 			// below rules were added by [steve]
 			add_rewrite_rule( $taxonomy_slug.'/(.+?)/date/([0-9]{4})/([0-9]{1,2})/?$', 'index.php?'.$taxonomy_key.'=$matches[1]&year=$matches[2]&monthnum=$matches[3]', 'top' );
 			add_rewrite_rule( $taxonomy_slug.'/(.+?)/date/([0-9]{4})/([0-9]{1,2})/page/?([0-9]{1,})/?$', 'index.php?'.$taxonomy_key.'=$matches[1]&year=$matches[2]&monthnum=$matches[3]&paged=$matches[4]', 'top' );
-
-			add_rewrite_rule( $slug.'/'.$taxonomy_slug.'/(.+?)/date/([0-9]{4})/([0-9]{1,2})/?$', 'index.php?'.$taxonomy_key.'=$matches[1]&year=$matches[2]&monthnum=$matches[3]&post_type='.$post_type, 'top' );
-			add_rewrite_rule( $slug.'/'.$taxonomy_slug.'/(.+?)/date/([0-9]{4})/([0-9]{1,2})/page/?([0-9]{1,})/?$', 'index.php?'.$taxonomy_key.'=$matches[1]&year=$matches[2]&monthnum=$matches[3]&paged=$matches[4]&post_type='.$post_type, 'top' );
+			add_rewrite_rule( $slug.'/'.$taxonomy_slug.'/(.+?)/date/([0-9]{4})/([0-9]{1,2})/?$', 'index.php?'.$taxonomy_key.'=$matches[1]&year=$matches[2]&monthnum=$matches[3]', 'top' );
+			add_rewrite_rule( $slug.'/'.$taxonomy_slug.'/(.+?)/date/([0-9]{4})/([0-9]{1,2})/page/?([0-9]{1,})/?$', 'index.php?'.$taxonomy_key.'=$matches[1]&year=$matches[2]&monthnum=$matches[3]&paged=$matches[4]', 'top' );
 
 			do_action( 'CPTP_registered_'.$taxonomy.'_rules', $object_type, $args, $taxonomy_slug );
 
