26f2929d2ba0cf20bdab6746836f7068fd7f633b
Hiroshi Urabe <mail@torounit.com>
2015-04-01 14:17:52 +0900

    test bug fix

--- tests/test-cptp-module-rewrite.php
+++ tests/test-cptp-module-rewrite.php
@@ -35,9 +35,19 @@ class CPTP_Module_Rewrite_Test extends WP_UnitTestCase {
 	 *
 	 * @test
 	 * @group rewrite
+	 * @runInSeparateProcess
+	 * @preserveGlobalState disabled
+	 *
 	 */
 	public function test_cpt_archive() {
 		register_post_type( $this->post_type, array( "public" => true , 'taxonomies' => array('category'), "has_archive" => true ) );
+
+		do_action('wp_loaded');
+		/** @var WP_Rewrite $wp_rewrite */
+		global $wp_rewrite;
+		$wp_rewrite->flush_rules();
+
+
 		$this->factory->post->create_many( 10 , array( 'post_type' => $this->post_type, "post_date" => "2012-12-12") );
 		$this->go_to( get_post_type_archive_link( $this->post_type ) );
 		$this->assertQueryTrue( "is_archive", "is_post_type_archive" );
@@ -49,6 +59,9 @@ class CPTP_Module_Rewrite_Test extends WP_UnitTestCase {
 	 *
 	 * @test
 	 * @group rewrite
+	 * @runInSeparateProcess
+	 * @preserveGlobalState disabled
+	 *
 	 */
 	public function test_cpt_date_archive() {
 
@@ -62,28 +75,30 @@ class CPTP_Module_Rewrite_Test extends WP_UnitTestCase {
 			"post_date" => "2012-12-12"
 		) );
 
-		add_action( 'wp_loaded', function() use ( $post_type_object ) {
+		do_action('wp_loaded');
+		/** @var WP_Rewrite $wp_rewrite */
+		global $wp_rewrite;
+		$wp_rewrite->flush_rules();
 
-			$this->go_to( home_url( "/" . $post_type_object->rewrite["slug"] . "/2012" ) );
-			$this->assertQueryTrue( "is_archive", "is_post_type_archive", "is_date", "is_year" );
+		$this->go_to( home_url( "/" . $post_type_object->rewrite["slug"] . "/2012" ) );
+		$this->assertQueryTrue( "is_archive", "is_post_type_archive", "is_date", "is_year" );
 
-			$this->go_to( next_posts( 0, false ) );
-			$this->assertQueryTrue( "is_archive", "is_post_type_archive", "is_date", "is_year", "is_paged" );
-
-
-			$this->go_to( home_url( "/" . $post_type_object->rewrite["slug"] . "/2012/12" ) );
-			$this->assertQueryTrue( "is_archive", "is_post_type_archive", "is_date", "is_month" );
-
-			$this->go_to( next_posts( 0, false ) );
-			$this->assertQueryTrue( "is_archive", "is_post_type_archive", "is_date", "is_month", "is_paged" );
+		$this->go_to( next_posts( 0, false ) );
+		$this->assertQueryTrue( "is_archive", "is_post_type_archive", "is_date", "is_year", "is_paged" );
 
 
-			$this->go_to( home_url( "/" . $post_type_object->rewrite["slug"] . "/2012/12/12" ) );
-			$this->assertQueryTrue( "is_archive", "is_post_type_archive", "is_date", "is_day" );
+		$this->go_to( home_url( "/" . $post_type_object->rewrite["slug"] . "/2012/12" ) );
+		$this->assertQueryTrue( "is_archive", "is_post_type_archive", "is_date", "is_month" );
 
-			$this->go_to( next_posts( 0, false ) );
-			$this->assertQueryTrue( "is_archive", "is_post_type_archive", "is_date", "is_day", "is_paged" );
-		});
+		$this->go_to( next_posts( 0, false ) );
+		$this->assertQueryTrue( "is_archive", "is_post_type_archive", "is_date", "is_month", "is_paged" );
+
+
+		$this->go_to( home_url( "/" . $post_type_object->rewrite["slug"] . "/2012/12/12" ) );
+		$this->assertQueryTrue( "is_archive", "is_post_type_archive", "is_date", "is_day" );
+
+		$this->go_to( next_posts( 0, false ) );
+		$this->assertQueryTrue( "is_archive", "is_post_type_archive", "is_date", "is_day", "is_paged" );
 
 	}
 
@@ -92,6 +107,9 @@ class CPTP_Module_Rewrite_Test extends WP_UnitTestCase {
 	 *
 	 * @test
 	 * @group rewrite
+	 * @runInSeparateProcess
+	 * @preserveGlobalState disabled
+	 *
 	 */
 	public function test_cpt_date_archive_with_date_front() {
 
@@ -102,27 +120,30 @@ class CPTP_Module_Rewrite_Test extends WP_UnitTestCase {
 
 		$this->factory->post->create_many( 10 , array( 'post_type' => $this->post_type, "post_date" => "2012-12-12") );
 
-		add_action( 'wp_loaded', function() use ( $post_type_object ) {
-			$this->go_to( home_url( "/" . $post_type_object->rewrite["slug"] . CPTP_Util::get_date_front( $this->post_type ) . "/2012" ) );
-			$this->assertQueryTrue( "is_archive", "is_post_type_archive", "is_date", "is_year" );
+		do_action('wp_loaded');
+		/** @var WP_Rewrite $wp_rewrite */
+		global $wp_rewrite;
+		$wp_rewrite->flush_rules();
 
-			$this->go_to( next_posts( 0, false ) );
-			$this->assertQueryTrue( "is_archive", "is_post_type_archive", "is_date", "is_year", "is_paged" );
+		$this->go_to( home_url( "/" . $post_type_object->rewrite["slug"] . CPTP_Util::get_date_front( $this->post_type ) . "/2012" ) );
+		$this->assertQueryTrue( "is_archive", "is_post_type_archive", "is_date", "is_year" );
 
-
-			$this->go_to( home_url( "/" . $post_type_object->rewrite["slug"] . CPTP_Util::get_date_front( $this->post_type ) . "/2012/12" ) );
-			$this->assertQueryTrue( "is_archive", "is_post_type_archive", "is_date", "is_month" );
-
-			$this->go_to( next_posts( 0, false ) );
-			$this->assertQueryTrue( "is_archive", "is_post_type_archive", "is_date", "is_month", "is_paged" );
+		$this->go_to( next_posts( 0, false ) );
+		$this->assertQueryTrue( "is_archive", "is_post_type_archive", "is_date", "is_year", "is_paged" );
 
 
-			$this->go_to( home_url( "/" . $post_type_object->rewrite["slug"] . CPTP_Util::get_date_front( $this->post_type ) . "/2012/12/12" ) );
-			$this->assertQueryTrue( "is_archive", "is_post_type_archive", "is_date", "is_day" );
+		$this->go_to( home_url( "/" . $post_type_object->rewrite["slug"] . CPTP_Util::get_date_front( $this->post_type ) . "/2012/12" ) );
+		$this->assertQueryTrue( "is_archive", "is_post_type_archive", "is_date", "is_month" );
 
-			$this->go_to( next_posts( 0, false ) );
-			$this->assertQueryTrue( "is_archive", "is_post_type_archive", "is_date", "is_day", "is_paged" );
-		});
+		$this->go_to( next_posts( 0, false ) );
+		$this->assertQueryTrue( "is_archive", "is_post_type_archive", "is_date", "is_month", "is_paged" );
+
+
+		$this->go_to( home_url( "/" . $post_type_object->rewrite["slug"] . CPTP_Util::get_date_front( $this->post_type ) . "/2012/12/12" ) );
+		$this->assertQueryTrue( "is_archive", "is_post_type_archive", "is_date", "is_day" );
+
+		$this->go_to( next_posts( 0, false ) );
+		$this->assertQueryTrue( "is_archive", "is_post_type_archive", "is_date", "is_day", "is_paged" );
 
 	}
 
@@ -131,6 +152,9 @@ class CPTP_Module_Rewrite_Test extends WP_UnitTestCase {
 	 *
 	 * @test
 	 * @group rewrite
+	 * @runInSeparateProcess
+	 * @preserveGlobalState disabled
+	 *
 	 */
 	public function test_cpt_author_archive() {
 		register_post_type( $this->post_type, array( "public" => true , 'taxonomies' => array('category'), "has_archive" => true ) );
@@ -142,13 +166,16 @@ class CPTP_Module_Rewrite_Test extends WP_UnitTestCase {
 		$user = get_userdata($user_id);
 		$user->user_nicename;
 
-		add_action( 'wp_loaded', function() use ( $post_type_object, $user ) {
-			$this->go_to( home_url( "/" . $post_type_object->rewrite["slug"] . "/author/" . $user->user_nicename ) );
-			$this->assertQueryTrue( "is_archive", "is_post_type_archive", "is_author" );
+		do_action('wp_loaded');
+		/** @var WP_Rewrite $wp_rewrite */
+		global $wp_rewrite;
+		$wp_rewrite->flush_rules();
 
-			$this->go_to( next_posts( 0, false ) );
-			$this->assertQueryTrue( "is_archive", "is_post_type_archive", "is_author", "is_paged" );
-		});
+		$this->go_to( home_url( "/" . $post_type_object->rewrite["slug"] . "/author/" . $user->user_nicename ) );
+		$this->assertQueryTrue( "is_archive", "is_post_type_archive", "is_author" );
+
+		$this->go_to( next_posts( 0, false ) );
+		$this->assertQueryTrue( "is_archive", "is_post_type_archive", "is_author", "is_paged" );
 
 	}
 
@@ -157,6 +184,9 @@ class CPTP_Module_Rewrite_Test extends WP_UnitTestCase {
 	 *
 	 * @test
 	 * @group rewrite
+	 * @runInSeparateProcess
+	 * @preserveGlobalState disabled
+	 *
 	 */
 	public function test_cpt_category_archive() {
 		register_taxonomy( $this->taxonomy, $this->post_type,  array( "public" => true , "rewrite" => array("slug" => rand_str( 12 ) )));
@@ -173,13 +203,17 @@ class CPTP_Module_Rewrite_Test extends WP_UnitTestCase {
 		}
 
 		$category_obj = get_category( $cat_id );
-		add_action( 'wp_loaded', function() use ( $post_type_object, $category_obj ) {
-			$this->go_to( home_url( "/" . $post_type_object->rewrite["slug"] . "/" . get_option( 'category_base' ) . "/" . $category_obj->slug ) );
-			$this->assertQueryTrue( "is_archive", "is_post_type_archive", "is_category" );
 
-			$this->go_to( next_posts( 0, false ) );
-			$this->assertQueryTrue( "is_archive", "is_post_type_archive", "is_category", "is_paged" );
-		});
+		do_action('wp_loaded');
+		/** @var WP_Rewrite $wp_rewrite */
+		global $wp_rewrite;
+		$wp_rewrite->flush_rules();
+
+		$this->go_to( home_url( "/" . $post_type_object->rewrite["slug"] . "/" . get_option( 'category_base' ) . "/" . $category_obj->slug ) );
+		$this->assertQueryTrue( "is_archive", "is_post_type_archive", "is_category" );
+
+		$this->go_to( next_posts( 0, false ) );
+		$this->assertQueryTrue( "is_archive", "is_post_type_archive", "is_category", "is_paged" );
 
 	}
 
