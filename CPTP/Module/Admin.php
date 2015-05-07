fa73d3cbdf2a2f2faf09c328ae864146b3555c11
Hiroshi Urabe <mail@torounit.com>
2015-05-07 11:14:23 +0900

    refactaring

--- CPTP/Module/Admin.php
+++ CPTP/Module/Admin.php
@@ -1,7 +1,6 @@
 <?php
 
 
-
 /**
  *
  * Admin Page View.
@@ -10,7 +9,6 @@
  * @since 0.9.4
  *
  * */
-
 class CPTP_Module_Admin extends CPTP_Module {
 
 	public function add_hook() {
@@ -34,35 +32,38 @@ class CPTP_Module_Admin extends CPTP_Module {
 		);
 
 		$post_types = CPTP_Util::get_post_types();
-		foreach ( $post_types as $post_type ):
 
-			add_settings_field( $post_type.'_structure',
+		foreach ( $post_types as $post_type ) {
+
+			add_settings_field(
+				$post_type . '_structure',
 				$post_type,
 				array( $this, 'setting_structure_callback_function' ),
 				'permalink',
 				'cptp_setting_section',
-				$post_type.'_structure'
+				array( 'label_for' => $post_type . '_structure', 'post_type' => $post_type )
 			);
-
-			register_setting( 'permalink',$post_type.'_structure' );
-		endforeach;
+			register_setting( 'permalink', $post_type . '_structure' );
+		}
 
 		add_settings_field(
 			'no_taxonomy_structure',
 			__( 'Use custom permalink of custom taxonomy archive.', 'cptp' ),
 			array( $this, 'setting_no_tax_structure_callback_function' ),
 			'permalink',
-			'cptp_setting_section'
+			'cptp_setting_section',
+			array( 'label_for' => 'no_taxonomy_structure' )
 		);
 
 		register_setting( 'permalink', 'no_taxonomy_structure' );
 
 		add_settings_field(
-			'add_post_type_for_tax.',
+			'add_post_type_for_tax',
 			__( 'Add post_type query for custom taxonomy archive.', 'cptp' ),
 			array( $this, 'add_post_type_for_tax_callback_function' ),
 			'permalink',
-			'cptp_setting_section'
+			'cptp_setting_section',
+			array( 'label_for' => 'add_post_type_for_tax' )
 		);
 
 		register_setting( 'permalink', 'no_taxonomy_structure' );
@@ -71,22 +72,23 @@ class CPTP_Module_Admin extends CPTP_Module {
 
 	public function setting_section_callback_function() {
 		?>
-			<p><?php _e( 'Setting permalinks of custom post type.', 'cptp' );?><br />
-			<?php _e( "The tags you can use is WordPress Structure Tags and '%\"custom_taxonomy_slug\"%'. (e.g. %actors%)", 'cptp' );?><br />
-			<?php _e( "%\"custom_taxonomy_slug\"% is replaced the taxonomy's term.'.", 'cptp' );?></p>
+		<p><?php _e( 'Setting permalinks of custom post type.', 'cptp' ); ?><br/>
+			<?php _e( "The tags you can use is WordPress Structure Tags and '%\"custom_taxonomy_slug\"%'. (e.g. %actors%)", 'cptp' ); ?>
+			<br/>
+			<?php _e( "%\"custom_taxonomy_slug\"% is replaced the taxonomy's term.'.", 'cptp' ); ?></p>
 
-			<p><?php _e( "Presence of the trailing '/' is unified into a standard permalink structure setting.", 'cptp' );?>
-			<p><?php _e( 'If <code>has_archive</code> is true, add permalinks for custom post type archive.', 'cptp' );?>
-			<?php _e( "If you don't entered permalink structure, permalink is configured /%postname%/'.", 'cptp' );?>
-			</p>
-		<?php
+		<p><?php _e( "Presence of the trailing '/' is unified into a standard permalink structure setting.", 'cptp' ); ?>
+		<p><?php _e( 'If <code>has_archive</code> is true, add permalinks for custom post type archive.', 'cptp' ); ?>
+			<?php _e( "If you don't entered permalink structure, permalink is configured /%postname%/'.", 'cptp' ); ?>
+		</p>
+	<?php
 	}
 
-	public function setting_structure_callback_function(  $option  ) {
+	public function setting_structure_callback_function( $option ) {
 
-		$post_type = str_replace( '_structure', '' ,$option );
-		$pt_object = get_post_type_object( $post_type );
-		$slug = $pt_object->rewrite['slug'];
+		$post_type  = $option['post_type'];
+		$pt_object  = get_post_type_object( $post_type );
+		$slug       = $pt_object->rewrite['slug'];
 		$with_front = $pt_object->rewrite['with_front'];
 
 		$value = CPTP_Util::get_permalink_structure( $post_type );
@@ -102,31 +104,23 @@ class CPTP_Module_Admin extends CPTP_Module {
 
 		global $wp_rewrite;
 		$front = substr( $wp_rewrite->front, 1 );
-		if (  $front and $with_front ) {
-			$slug = $front.$slug;
+		if ( $front and $with_front ) {
+			$slug = $front . $slug;
 		}
-
-		echo '<p><code>'.home_url().'/'.esc_html( $slug ).'</code> <input name="'.esc_attr( $option ).'" id="'.esc_attr( $option ).'" type="text" class="regular-text code '.$this->disabled_string($disabled).'" value="' . $value .'" '.$this->disabled_string($disabled).' /></p>';
-		echo '<p>has_archive: <code>';
-		echo $pt_object->has_archive ? 'true' : 'false';
-		echo '</code> / ';
-		echo 'with_front: <code>';
-		echo $pt_object->rewrite['with_front'] ? 'true' : 'false';
-		echo '</code></p>';
-
+		?>
+		<p>
+			<code><?php echo home_url() . '/' . esc_html( $slug );?></code>
+			<input name="<?php echo esc_attr( $option );?>" id="<?php echo esc_attr( $option );?>" type="text" class="regular-text code " value="<?php echo  esc_attr( $value ) ;?>" <?php  disabled( $disabled, true, true );?> />
+		</p>
+		<p>has_archive: <code><?php echo $pt_object->has_archive ? 'true' : 'false';?></code> / with_front: <code><?php echo $pt_object->rewrite['with_front'] ? 'true' : 'false';?></code></p>
+		<?php
 	}
 
-	private function disabled_string( $bool ) {
-		if ( $bool ) {
-			return 'disabled';
-		}
-		return '';
-	}
 
-	public function setting_no_tax_structure_callback_function(){
+	public function setting_no_tax_structure_callback_function() {
 		echo '<input name="no_taxonomy_structure" id="no_taxonomy_structure" type="checkbox" value="1" class="code" ' . checked( false, get_option( 'no_taxonomy_structure' ), false ) . ' /> ';
 		$txt = __( "If you check,The custom taxonomy's permalinks is <code>%s/post_type/taxonomy/term</code>.", 'cptp' );
-		printf( $txt , home_url() );
+		printf( $txt, home_url() );
 	}
 
 
@@ -135,7 +129,6 @@ class CPTP_Module_Admin extends CPTP_Module {
 		_e( "custom taxonomy archive also works as post type archive.", 'cptp' );
 		_e( "There are cases when template to be loaded is changed.", 'cptp' );
 	}
-
 
 
 	/**
@@ -150,7 +143,6 @@ class CPTP_Module_Admin extends CPTP_Module {
 	}
 
 
-
 	/**
 	 *
 	 * add js for pointer
@@ -160,23 +152,23 @@ class CPTP_Module_Admin extends CPTP_Module {
 		if ( ! is_network_admin() ) {
 			$dismissed = explode( ',', get_user_meta( get_current_user_id(), 'dismissed_wp_pointers', true ) );
 			if ( false === array_search( 'cptp_pointer0871', $dismissed ) ) {
-				$content = __( "<h3>Custom Post Type Permalinks</h3><p>From <a href='options-permalink.php'>Permalinks</a>, set a custom permalink for each post type.</p>" , 'cptp' );
-			?>
+				$content = __( "<h3>Custom Post Type Permalinks</h3><p>From <a href='options-permalink.php'>Permalinks</a>, set a custom permalink for each post type.</p>", 'cptp' );
+				?>
 				<script type="text/javascript">
-				jQuery(function($) {
+					jQuery(function ($) {
 
-					$("#menu-settings .wp-has-submenu").pointer({
-						content: "<?php echo $content;?>",
-						position: {"edge":"left","align":"center"},
-						close: function() {
-							$.post( 'admin-ajax.php', {
-								action:'dismiss-wp-pointer',
-								pointer: 'cptp_pointer0871'
-							})
+						$("#menu-settings .wp-has-submenu").pointer({
+							content: "<?php echo $content;?>",
+							position: {"edge": "left", "align": "center"},
+							close: function () {
+								$.post('admin-ajax.php', {
+									action: 'dismiss-wp-pointer',
+									pointer: 'cptp_pointer0871'
+								})
 
-						}
-					}).pointer("open");
-				});
+							}
+						}).pointer("open");
+					});
 				</script>
 			<?php
 			}
