58949e87c0e0766201be237cee23b35b3ac37eec
Hiroshi Urabe <mail@torounit.com>
2015-05-07 10:42:01 +0900

    check nonce on save.

--- CPTP/Module/Option.php
+++ CPTP/Module/Option.php
@@ -1,7 +1,6 @@
 <?php
 
 
-
 /**
  *
  * Options.
@@ -12,7 +11,6 @@
  * @since 0.9.6
  *
  * */
-
 class CPTP_Module_Option extends CPTP_Module {
 
 	public function add_hook() {
@@ -22,40 +20,58 @@ class CPTP_Module_Option extends CPTP_Module {
 
 	public function save_options() {
 
-		if ( isset( $_POST['submit'] ) and isset( $_POST['_wp_http_referer'] ) ){
-			if ( false !== strpos( $_POST['_wp_http_referer'], 'options-permalink.php' ) ) {
-				$post_types = CPTP_Util::get_post_types();
-				foreach ( $post_types as $post_type ):
-
-					$structure = trim( esc_attr( $_POST[ $post_type.'_structure' ] ) );#get setting
-
-					#default permalink structure
-					if ( ! $structure ) {
-						$structure = CPTP_DEFAULT_PERMALINK;
-					}
-
-					$structure = str_replace( '//', '/', '/'.$structure );# first "/"
-					#last "/"
-					$lastString = substr( trim( esc_attr( $_POST['permalink_structure'] ) ), -1 );
-					$structure = rtrim( $structure, '/' );
-
-					if ( $lastString == '/' ) {
-						$structure = $structure.'/';
-					}
-
-					update_option( $post_type.'_structure', $structure );
-				endforeach;
-			}
-			update_option( 'no_taxonomy_structure', ! isset( $_POST['no_taxonomy_structure'] ) );
-			update_option( 'add_post_type_for_tax', isset( $_POST['add_post_type_for_tax'] ) );
-
-
+		if ( empty( $_POST['submit'] ) ) {
+			return false;
 		}
+
+		if ( empty( $_POST['_wp_http_referer'] ) ) {
+			return false;
+		}
+
+		if ( false === strpos( $_POST['_wp_http_referer'], 'options-permalink.php' ) ) {
+			return false;
+		}
+
+		if ( empty ( $_POST['_wpnonce'] ) ) {
+			return false;
+		}
+		$nonce = $_POST['_wpnonce'];
+
+		if ( ! wp_verify_nonce( $nonce, 'update-permalink' ) ) {
+			return false;
+		}
+
+		$post_types = CPTP_Util::get_post_types();
+
+		foreach ( $post_types as $post_type ):
+
+			$structure = trim( esc_attr( $_POST[ $post_type . '_structure' ] ) );#get setting
+
+			#default permalink structure
+			if ( ! $structure ) {
+				$structure = CPTP_DEFAULT_PERMALINK;
+			}
+
+			$structure = str_replace( '//', '/', '/' . $structure );# first "/"
+			#last "/"
+			$lastString = substr( trim( esc_attr( $_POST['permalink_structure'] ) ), - 1 );
+			$structure  = rtrim( $structure, '/' );
+
+			if ( $lastString == '/' ) {
+				$structure = $structure . '/';
+			}
+
+			update_option( $post_type . '_structure', $structure );
+		endforeach;
+
+		update_option( 'no_taxonomy_structure', ! isset( $_POST['no_taxonomy_structure'] ) );
+		update_option( 'add_post_type_for_tax', isset( $_POST['add_post_type_for_tax'] ) );
+
 	}
 
 	public static function uninstall_hook() {
 		foreach ( CPTP_Util::get_post_types() as $post_type ) {
-			delete_option( $post_type.'_structure' );
+			delete_option( $post_type . '_structure' );
 		}
 
 		delete_option( 'no_taxonomy_structure' );
