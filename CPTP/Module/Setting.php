05934652f5052d9bd9853fe1dd1e7173602d7193
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2017-02-04 03:05:49 +0900

    for upgrade

--- CPTP/Module/Setting.php
+++ CPTP/Module/Setting.php
@@ -8,29 +8,48 @@
  * @package Custom_Post_Type_Permalinks
  * @since 0.9.4
  * */
-
-
 class CPTP_Module_Setting extends CPTP_Module {
 
 	public function add_hook() {
+		$this->update_version();
 		add_action( 'init', array( $this, 'load_textdomain' ) );
-		$this->check_version();
+		add_action( 'upgrader_process_complete', array( $this, 'upgrader_process_complete' ) );
 	}
 
 	/**
-	 *
 	 * check_version
 	 *
 	 * @since 0.8.6
 	 */
 
-	public function check_version() {
+	public function update_version() {
 		update_option( 'cptp_version', CPTP_VERSION );
 	}
 
+	/**
+	 * After update complete.
+	 *
+	 * @since 2.3.0
+	 *
+	 * @param object $wp_upgrader WP_Upgrader instance.
+	 * @param array  $options      Extra information about performed upgrade.
+	 */
+	public function upgrader_process_complete( $wp_upgrader, $options ) {
+
+		if ( ! is_array( $options['plugins'] ) ) {
+			return;
+		}
+
+		$plugin_path = plugin_basename( CPTP_PLUGIN_FILE );
+		if ( $options['action'] == 'update' && $options['type'] == 'plugin' ) {
+			if ( in_array( $plugin_path, $options['plugins'] ) ) {
+				//for update code.
+				add_option( 'no_taxonomy_structure', false );
+			}
+		}
+	}
 
 	/**
-	 *
 	 * load textdomain
 	 *
 	 * @since 0.6.2
