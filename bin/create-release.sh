e7b9d85fd08e1e2c4fe86e8f1c69d49acfa7d43f
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2017-11-16 12:28:15 +0900

    release command no update Stable Tag on beta.

--- bin/create-release.sh
+++ bin/create-release.sh
@@ -9,6 +9,10 @@ fi
 
 version=$1
 
-sed -i '' -e "s/^Stable tag: .*/Stable tag: ${version}/g" readme.txt;
+if [ ! `echo $version | grep -e 'alpha' -e 'beta' -e 'RC' -e 'rc'` ] ; then
+  sed -i '' -e "s/^Stable tag: .*/Stable tag: ${version}/g" readme.txt;
+fi
+
+
 sed -i '' -e "s/^ \* Version: .*/ * Version: ${version}/g" custom-post-type-permalinks.php;
 sed -i '' -e "s/^ \* @version .*/ * @version ${version}/g" custom-post-type-permalinks.php;
