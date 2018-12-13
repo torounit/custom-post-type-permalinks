0440ee41016aff4a2e3f616b93045d391aa99bc7
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2018-12-13 22:22:45 +0900

    fix test suite

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,8 +6,14 @@
  */
 
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
+
 if ( ! $_tests_dir ) {
-	$_tests_dir = '/tmp/wordpress-tests-lib';
+	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
+}
+
+if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
+	echo "Could not find $_tests_dir/includes/functions.php, have you run bin/install-wp-tests.sh ?" . PHP_EOL;
+	exit( 1 );
 }
 
 // Give access to tests_add_filter() function.
@@ -19,7 +25,6 @@ require_once $_tests_dir . '/includes/functions.php';
 function _manually_load_plugin() {
 	require dirname( dirname( __FILE__ ) ) . '/custom-post-type-permalinks.php';
 }
-
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
 // Start up the WP testing environment.
