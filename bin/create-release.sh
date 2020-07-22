46b737d15bf6f93a082042eeb3d036c2dd4bb2ef
Hiroshi Urabe <mail@torounit.com>
2020-07-22 17:02:17 +0900

    fix build task

--- bin/create-release.sh
+++ bin/create-release.sh
@@ -9,10 +9,6 @@ fi
 
 version=$1
 
-if [ ! `echo $version | grep -e 'alpha' -e 'beta' -e 'RC' -e 'rc'` ] ; then
-  sed -i '' -e "s/^Stable tag: .*/Stable tag: ${version}/g" readme.txt;
-fi
-
-
+sed -i '' -e "s/^Stable tag: .*/Stable tag: ${version}/g" readme.md;
 sed -i '' -e "s/^ \* Version: .*/ * Version: ${version}/g" custom-post-type-permalinks.php;
 sed -i '' -e "s/^ \* @version .*/ * @version ${version}/g" custom-post-type-permalinks.php;
