08ef22c68d23437be97974933e2ec015ef81fa14
Hiroshi Urabe <mail@torounit.com>
2021-01-04 17:28:07 +0900

    Release 3.4.4

--- custom-post-type-permalinks.php
+++ custom-post-type-permalinks.php
@@ -5,13 +5,13 @@
  * Description:  Add post archives of custom post type and customizable permalinks.
  * Author: Toro_Unit
  * Author URI: https://torounit.com/
- * Version: 3.4.3
+ * Version: 3.4.4
  * Text Domain: custom-post-type-permalinks
  * License: GPL2 or later
  * Domain Path: /language/
  *
  * @package Custom_Post_Type_Permalinks
- * @version 3.4.3
+ * @version 3.4.4
  */
 
 define( 'CPTP_PLUGIN_FILE', __FILE__ );
